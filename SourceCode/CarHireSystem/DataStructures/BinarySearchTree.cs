60f6ba17ba74e0542634266e80f3fb6674c05ce1
Dean-Cimatu <deancimatu@gmail.com>
2026-03-15 22:53:21 +0000

    Added BST SearchByriceRange method

--- SourceCode/CarHireSystem/DataStructures/BinarySearchTree.cs
+++ SourceCode/CarHireSystem/DataStructures/BinarySearchTree.cs
@@ -1,6 +1,90 @@
-namespace CarHireSystem.Models;
+namespace CarHireSystem.DataStructures;
+using CarHireSystem.Models;
 
 public class BinarySearchTree
 {
-    
+    private class Node
+    {
+        public Car Data { get; set; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+
+        public Node(Car car)
+        {
+            Data = car;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    private Node? _root;
+
+    public void Insert(Car car)
+    {
+        if (_root == null)
+        {
+            _root = new Node(car);
+        }
+        else
+        {
+            Node current = _root;
+            while (true)
+            {
+                if (car.PricePerDay < current.Data.PricePerDay)
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = new Node(car);
+                        return;
+                    }
+                    else
+                    {
+                        current = current.Left;
+                    }
+                }
+                else if (car.PricePerDay > current.Data.PricePerDay)
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = new Node(car);
+                        return;
+                    }
+                    else
+                    {
+                        current = current.Right;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    public List<Car> SearchByPriceRange(decimal min, decimal max)
+    {
+        List<Car> results = new List<Car>();
+        SearchRecursive(_root, min, max, results);
+        return results;
+    }
+    private void SearchRecursive(Node? node, decimal min, decimal max, List<Car> results)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        if (min <= node.Data.PricePerDay && node.Data.PricePerDay <= max)
+        {
+            results.Add(node.Data);
+        }
+        if (min < node.Data.PricePerDay)
+        {
+            SearchRecursive(node.Left, min, max, results);
+        }
+        if (max > node.Data.PricePerDay)
+        {
+            SearchRecursive(node.Right, min, max, results);
+        }
+    }
 }
