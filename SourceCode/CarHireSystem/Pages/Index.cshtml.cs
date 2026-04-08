82b39080eecc01a59b45435dccedce8c3bd58c29
Dean-Cimatu <deancimatu@gmail.com>
2026-04-08 11:14:13 +0100

    Added rotating car display on home

--- SourceCode/CarHireSystem/Pages/Index.cshtml.cs
+++ SourceCode/CarHireSystem/Pages/Index.cshtml.cs
@@ -1,12 +1,26 @@
-using Microsoft.AspNetCore.Mvc;
+using CarHireSystem.Database;
+using CarHireSystem.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarHireSystem.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
-    {
+    private readonly CarHireDbContext _db;
 
+    public IndexModel(CarHireDbContext db)
+    {
+        _db = db;
+    }
+
+    public Car[] AvailableCars { get; set; } = Array.Empty<Car>();
+
+    public async Task OnGetAsync()
+    {
+        AvailableCars = await _db.Cars
+            .Where(c => c.IsAvailable)
+            .OrderBy(c => c.PricePerDay)
+            .ToArrayAsync();
     }
 }
