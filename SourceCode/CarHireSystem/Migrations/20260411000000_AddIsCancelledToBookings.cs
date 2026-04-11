4f6b729756de11a1c05064e0b086fc28605e851d
Dean-Cimatu <deancimatu@gmail.com>
2026-04-11 10:57:40 +0100

    Added pagination to admin booking table

---
+++ SourceCode/CarHireSystem/Migrations/20260411000000_AddIsCancelledToBookings.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarHireSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCancelledToBookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCancelled",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCancelled",
+                table: "Bookings");
+        }
+    }
+}
